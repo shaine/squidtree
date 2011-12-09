a51405d89ab05cd94a3d35c6a274b8a35aebd800
Shaine Hatch <hatch@fadelight.com>
2011-12-08 19:04:41 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squidtree::Application.config.secret_token = '722ceb6a5ccc56519d0f6066f1c56fa765519375d0144085e085984a4468f60d7ee277db3c1103d35a0d9c00b4971aa07a36031746a126d97d7a8efc89c510b4'
