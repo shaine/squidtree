854ea75eb3dec9064da26f01c0e7e931b66c3611
Shaine Hatch <hatch@fadelight.com>
2011-12-15 16:40:19 -0700

    Post render partial working

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,6 @@
 class BlogController < ApplicationController
   def index
-    @posts = Post.sort(:created_at.desc).limit(10)
-    puts @posts.count
+    @posts = Post.all(:sort=>"created_at desc", :limit=>10)
   end
 
   def view
