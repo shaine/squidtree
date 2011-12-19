8b889c253e94ae078a47bc7864ca58d7c4f8ad46
Shaine Hatch <hatch@fadelight.com>
2011-12-18 19:04:49 -0700

    Implemented tag list

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,12 @@
 module BlogHelper
+  def tag_list(tags, length = 0)
+    tag_list_string = ""
+    tags.each do |tag|
+      if !length || strip_tags(tag_list_string + " #{tags}").strip.length < length
+        tag_list_string += "&nbsp; <a href=\"/blog/tags/#{URI.escape(tag.to_url)}\">#{tag}</a>"
+      end
+    end
+    
+    tag_list_string.html_safe
+  end
 end
