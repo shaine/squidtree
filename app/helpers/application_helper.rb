8d99a1867c31e8c693d57a67c6fea3949c8f83b8
Shaine Hatch <hatch@fadelight.com>
2011-12-23 00:04:28 -0700

    Heading change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+  
+  def page_title(page_title)
+    content_for(:page_title) { page_title }
+  end
 end
