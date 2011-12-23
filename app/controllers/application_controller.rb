967141144d3e695d3459448e0649b797b8812e99
Shaine Hatch <hatch@fadelight.com>
2011-12-23 12:39:48 -0700

    Colors can override default page color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :start_style_date
+ 
+  private
+ 
+  def start_style_date
+    @color_date = Colorable.day_of_year
+  end
 end
