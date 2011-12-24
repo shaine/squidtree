4f116cd4e93a6d905d08a4071f6697663e6fd5b0
Shaine Hatch <hatch@fadelight.com>
2011-12-24 12:01:03 -0700

    AssetsRAILS_ENV=production rake assets:precompile

--- app/models/ar_tag.rb
+++
@@ -1,4 +0,0 @@
-class ArTag < ActiveRecord::Base
-  set_table_name "tags"
-  has_and_belongs_to_many :posts, :class_name => 'ArPost', :foreign_key=>'tag_id', :join_table=>'posts_tags', :association_foreign_key=>'post_id'
-end
