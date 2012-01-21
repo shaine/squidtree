9ce036aa291b898fc8fbf955b032a4f2239429a5
Shaine Hatch <hatch@fadelight.com>
2012-01-21 11:52:36 -0700

    Trying to get cukes running again

--- app/cells/posts_cell.rb
+++ app/cells/posts_cell.rb
@@ -3,15 +3,18 @@ class PostsCell < Cell::Rails
   def index
     @months = []
     i = 0
-    while @months.length < 12
-      date = Time.now.months_ago(i)
-      first = date.at_beginning_of_month
-      last = date.at_end_of_month
-      if Post.all(:created_at.gte => first, :created_at.lt => last).count > 0
-        @months << first
+    if Post.count
+      while @months.length < 12
+        date = Time.now.months_ago(i)
+        first = date.at_beginning_of_month
+        last = date.at_end_of_month
+        if Post.all(:created_at.gte => first, :created_at.lt => last).count > 0
+          @months << first
+        end
+        i += 1
       end
-      i += 1
     end
+    
     
     render
   end
