2e5c12ed6669a0c67ae82b6f6f308468a5f96086
Shaine Hatch <hatch@fadelight.com>
2012-01-21 14:08:50 -0700

    Nil DB Cleaner

--- features/support/env.rb
+++ features/support/env.rb
@@ -33,12 +33,12 @@ ActionController::Base.allow_rescue = false
 
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
-#begin
-#  require 'database_cleaner'
-#  require 'database_cleaner/cucumber'
-#  DatabaseCleaner.strategy = :truncation
-#rescue NameError
-#  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-#end
+begin
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
+  DatabaseCleaner.strategy = nil
+rescue NameError
+  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+end
 
 #Cucumber::Rails::Database.javascript_strategy = :truncation
