754c6f130286757a842bc21556e6f23d34434393
Shaine Hatch <hatch@fadelight.com>
2012-01-31 20:16:11 -0700

    Precompile finally works

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  #Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Squidtree
@@ -48,12 +48,14 @@ module Squidtree
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    require "#{Rails.root}/lib/assets/colorable.rb"
   end
 end
