606288f069ed85259d89354e19a783e014f0cd42
Shaine Hatch <shaine@digitaltrike.com>
2012-05-15 22:49:07 -0600

    Cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,27 @@
 class User
   include MongoMapper::Document
   plugin MongoMapper::Plugins::Sluggable
-  
+
   key :slug, String
   key :first_name, String
   key :last_name, String
   key :alias, String
   timestamps!
-  
+
   # Relationships.
   many :comments
   many :links
   many :posts
-  
+
   # Validations.
   validates_presence_of :first_name, :last_name, :slug
-  
+
   sluggable :alias, :method => :to_url, :index => false
-  
+
   def to_param
     slug # or whatever you set :url_attribute to
   end
-  
+
   def name
     first_name + ' ' + last_name
   end
