5f72733f15fd76b977a99d90a11fbcec6fc8cadc
Shaine Hatch <shaine@digitaltrike.com>
2012-05-15 23:56:11 -0600

    User object from login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,15 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    render :text => auth_hash.inspect
+    @user = User.find_by_uid(auth_hash["uid"])
+    if @user
+      render :text => "Welcome back #{@user.name}! You have already signed up."
+    else
+      @user = User.new :first_name => auth_hash["user_info"]["first_name"], :last_name => auth_hash["user_info"]["last_name"], :email => auth_hash["user_info"]["email"], :uid => auth_hash["uid"]
+      @user.save
+
+      render :text => "Hi #{@user.name}! You've signed up."
+    end
   end
 
   def failure
