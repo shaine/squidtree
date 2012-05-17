6259a9ce05273204896c710846da0515755d3868
Shaine Hatch <shaine@digitaltrike.com>
2012-05-16 18:51:36 -0600

    More omniauth updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,14 +9,22 @@ class SessionsController < ApplicationController
     if @user
       render :text => "Welcome back #{@user.name}! You have already signed up."
     else
-      @user = User.new :first_name => auth_hash["user_info"]["first_name"], :last_name => auth_hash["user_info"]["last_name"], :email => auth_hash["user_info"]["email"], :uid => auth_hash["uid"]
-      @user.save
+      @user = User.new :first_name => auth_hash["info"]["first_name"], :last_name => auth_hash["info"]["last_name"], :email => auth_hash["info"]["email"], :uid => auth_hash["uid"]
+      @user.alias = @user.name
 
       render :text => "Hi #{@user.name}! You've signed up."
     end
+
+    puts auth_hash
+    session[:user_id] = @user.id
   end
 
   def failure
+    render :text => "Sorry, but you didn't allow access to our app!"
   end
 
+  def destroy
+    session[:user_id] = nil
+    render :text => "You've logged out!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Squidtree::Application.routes.draw do
   get   '/login', :to => 'sessions#new', :as => :login
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/auth/failure', :to => 'sessions#failure'
+  get '/logout', :to => 'sessions#destroy'
 
   resources :links
 
