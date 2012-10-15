726aaf679b19327e36b176881807dea1d6806663
Shaine Hatch <shaine@digitaltrike.com>
2012-10-14 21:43:54 -0600

    Start login test

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,18 +1,26 @@
 Given /^I am signed in with provider "([^"]*)"(?:| as ([.+]))?$/ do |provider, user|
   OmniAuth.config.test_mode = true
 
-  OmniAuth.config.mock_auth[:google] = {
-    "provider"=>"google",
-    "uid"=>"http://digitaltrike.com/openid?id=112298941776999335504"
-  }
+  case provider
+  when "Facebook"
+    OmniAuth.config.mock_auth[:facebook] = {
+      "provider"  => "facebook",
+      "uid"       => '12345',
+      "info" => {
+        "email" => "testadmin@squidtree.com",
+        "first_name" => "Admin",
+        "last_name" => "Test",
+      }
+    }
 
-  case user
-  when "an admin"
-    OmniAuth.config.mock_auth[:google][:user_info] = {"email"=>"admin@digitaltrike.com", "first_name"=>"Admin", "last_name"=>"Test", "name"=>"Admin Test"}
-  when "a manager"
-    OmniAuth.config.mock_auth[:google][:user_info] = {"email"=>"manager@digitaltrike.com", "first_name"=>"Manager", "last_name"=>"Test", "name"=>"Manager Test"}
-  else # Or a user
-    OmniAuth.config.mock_auth[:google][:user_info] = {"email"=>"user1@digitaltrike.com", "first_name"=>"User", "last_name"=>"First", "name"=>"User First"}
+    case user
+    when "an admin"
+      OmniAuth.config.mock_auth[:facebook][:user_info] = {"email"=>"", "first_name"=>"Admin", "last_name"=>"Test", "name"=>"Admin Test"}
+    else # Or a user
+      OmniAuth.config.mock_auth[:facebook][:user_info] = {"email"=>"user1@digitaltrike.com", "first_name"=>"User", "last_name"=>"First", "name"=>"User First"}
+    end
+  else
+    raise "Provider #{provider} has not been configured."
   end
 
   visit "/auth/#{provider.downcase}_apps"
