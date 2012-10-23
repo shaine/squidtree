2f3d39210c7a05e895b71a729171c82e14a256da
Shaine Hatch <shaine@digitaltrike.com>
2012-10-23 14:49:49 -0600

    Add login/logout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Squidtree::Application.routes.draw do
-  get '/login', :to => 'sessions#new', :as => :login
+  match '/login' => redirect("/auth/facebook")
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/auth/failure', :to => 'sessions#failure'
   get '/logout', :to => 'sessions#destroy'
