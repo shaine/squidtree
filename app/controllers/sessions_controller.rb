deb8449832304070e6413bee5bf18cc775fb97a1
Shaine Hatch <shaine@digitaltrike.com>
2012-10-24 14:45:55 -0600

    Update/create users when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,34 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Extract user details from hash
     auth_hash = request.env['omniauth.auth']
+    user_details = {
+      :first_name => auth_hash["info"]["first_name"],
+      :last_name => auth_hash["info"]["last_name"],
+      :alias => auth_hash["info"]["nickname"],
+      :email => auth_hash["info"]["email"],
+      :uid => auth_hash["uid"]
+    }
 
+    # Try to find existing or old user
     user = User.find_by_uid(auth_hash["uid"])
+    user ||= User.find_by_first_name_and_last_name_and_uid(
+      user_details[:first_name],
+      user_details[:last_name],
+      nil
+    )
+
+    # Set user details
     if !user
-      user = User.new :first_name => auth_hash["info"]["first_name"], :last_name => auth_hash["info"]["last_name"], :email => auth_hash["info"]["email"], :uid => auth_hash["uid"]
-      user.alias = user.name
+      user = User.new user_details
+    elsif !user.valid?
+      user.update_attributes user_details
+    end
+
+    # Save user
+    unless user.save
+      flash[:notice] = "User account failed to save."
     end
 
     session[:user_id] = user.id
