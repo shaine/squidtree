98e098154082a40234d3c396c02e2478649065c6
Shaine Hatch <shaine@digitaltrike.com>
2012-11-01 20:25:42 -0600

    Start implementing error filtering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
+  end
+
   before_filter :init
   after_filter :remember_request
 
@@ -33,6 +38,13 @@ class ApplicationController < ActionController::Base
       redirect_to session[:previous_page]
     rescue ActionController::RedirectBackError
       redirect_to root_path
+    end
+  end
+
+  def render_error(status, exception)
+    respond_to do |format|
+      format.html { render template: "errors/error_#{status}", layout: "layouts/application", status: status }
+      format.all { render nothing: true, status: status }
     end
   end
 end
