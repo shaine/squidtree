1a8146db8a5d052c0ba5f807f3d8a389bea12ee7
Shaine Hatch <shaine@digitaltrike.com>
2012-11-01 20:31:08 -0600

    Markdown rendering for comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,25 @@
+class MarkdownRenderer < Redcarpet::Render::HTML
+  def link(link, title, alt_text)
+    "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
+  end
+  def autolink(link, link_type)
+    "<a target=\"_blank\" href=\"#{link}\">#{link}</a>"
+  end
+end
+
 module CommentsHelper
   def format_comment(comment)
     unless comment.is_old?
-      rndr = Redcarpet::Render::HTML.new(:filter_html => true, :no_styles => true)
-      mkdn = Redcarpet::Markdown.new rndr
+      rndr = MarkdownRenderer.new(
+        :filter_html => true,
+        :no_styles => true
+      )
+      mkdn = Redcarpet::Markdown.new rndr, {
+        :autolink => true,
+        :link_attributes => {
+          :target => "_new"
+        }
+      }
       mkdn.render(comment.content.gsub("#", "\\#")).html_safe
     else
       simple_format comment.content
