8cca4fde0eef2d69b0a8eb7ec96c332a553bfafd
Shaine Hatch <shaine@digitaltrike.com>
2012-11-20 23:20:41 -0700

    Fix status codes

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,13 @@
 class ErrorsController < ApplicationController
   def error_404
+    render :error_404, :status => 404
   end
 
   def error_403
+    render :error_403, :status => 403
   end
 
   def error_500
+    render :error_500, :status => 500
   end
 end
