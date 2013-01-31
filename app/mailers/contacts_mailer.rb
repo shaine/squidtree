93068ca5b4ed1ce6dde7bf5ddb896a3a35d6264d
Shaine Hatch <shaine@digitaltrike.com>
2013-01-30 20:41:45 -0700

    Contact mailer generated

---
+++ app/mailers/contacts_mailer.rb
@@ -0,0 +1,14 @@
+class ContactsMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contacts_mailer.contact.subject
+  #
+  def contact
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
