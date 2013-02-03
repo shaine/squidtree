e545a3d36075069d89eb17e43eed07c43d0373b5
Shaine Hatch <shaine@digitaltrike.com>
2013-02-02 17:43:42 -0700

    Implemented Microdata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def page_title(page_title)
     content_for(:page_title) { page_title }
   end
+
+  def page_type(page_type)
+    content_for(:page_type) { page_type }
+  end
 end
