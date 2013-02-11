ca66ba7279e17e993376249337c73d4e1ec0b25f
Shaine Hatch <shaine@digitaltrike.com>
2013-02-10 21:53:29 -0700

    Various design tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
       @links = Link.all(sort:"created_at desc", limit:20)
     end
     @search = params["search"]
-    @comment_logs = SiteActivity.all(conditions: {loggable_type: "Comment"}, limit:3, sort:"created_at DESC")
+    @comment_logs = SiteActivity.all(conditions: {loggable_type: "Comment"}, limit: 3, sort: "created_at DESC")
 
     if params[:page] && params[:page].to_i > 1
       @outer_title = "Page #{params[:page]}"
